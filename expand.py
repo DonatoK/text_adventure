8b63ab9e7dc9b8be59e214377cb8f6cec44b352a
DonatoK <donatokava@gmail.com>
2022-06-04 13:35:13 -0600

    clean function commit before testing

--- expand.py
+++ expand.py
@@ -11,11 +11,20 @@ def newFunction(option):
     # it right or not.
     print("The user input:", option)
     localUserInput = input("please enter something new")
+    print(localUserInput)
+    # getting  a user input using a 3 option function
+    optionList = ["cup", "orange", "banana"]
+    # before calling threeOptions make sure to print out the question for the user
+    print("There are three things on the table what would you like?")
+    localUserInput = threeOptions(optionList)
+    # Printing the prompt before and the result after allows for function reuse
     return localUserInput
 
 
 def threeOptions(threeItemList):
     # this function will take in a 3 item list, display the options, ask a User
     # For an input and return the selected option
+    print(threeItemList[0], ": 1\n", threeItemList[1],
+          ": 2\n", threeItemList[2], ": 3\n")
     userSelection = input()
     return userSelection
