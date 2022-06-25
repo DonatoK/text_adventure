26ea84dacdcf0ba6baff2e134dc358abc1daf5c3
DonatoK <donatokava@gmail.com>
2022-06-25 08:25:44 -0400

    predebug

--- a_game_module.py
+++ a_game_module.py
@@ -4,6 +4,47 @@
 peppers_health = 100
 bug_health = 100
 damage = 1
-def peppers_day():
 
+def peppers_day():
+    print("""Pepper is a black cat and expert bug hunter...
+
+            you are a stink bug...
+
+            Survive!!!""")
+    print("--------------------------------------------------")
+    input("press enter to continue")
+
+    print("""Stinkbug: You wake up on a wall, time to go do stink bug things,
+              your favorite. Makes you think back to the way your stink bug mom
+              would always tell you, first she would shout...""")
+
+    userNAME= input("enter your name")
+    print("Wait, she called you ",userNAME, "?")
+
+    userInput=input("Y?, N?")
+
+    if(userInput == "N" || "n")
+        while(userInput != "Y" || "y")
+            print("So what did she call you?")
+            userNAME= input("enter your name")
+            print("Wait, she called you ",userNAME, "?")
+            userInput=input("Y?, N?")
+
+    print("okay your mom would say ", userNAME,"""! You know stink bug life is
+           mostly about being the most annoying thing inside a house it can
+           be hard or it can be easy!""""
+
+    print("How difficult do you want this game to be?")
+    print("Easy")  #baseline
+    print("Hard")  #multiply extreme
+    print("EXTREME!!") # game ends
+
+    userInput=input()
+    print("Wow",userInput)
+
+    print("you ", UserName, """a stinkbug start to move, pepper notices,
+    the fight is on.
+    Do you run or do you fight?""")
+
+    userInput = input("run?, fight")
     return game_module_over
